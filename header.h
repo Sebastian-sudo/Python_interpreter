a5926b36e61bd295ef6f6e526bf3d4ccd5970169
Sebastian Mazurek <smazurek.sm@gmail.com>
2021-01-06 03:04:26 +0100

    added headers

---
+++ header.h
@@ -0,0 +1,13 @@
+#ifndef HEADERS
+#define HEADERS
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <array>
+
+#endif
